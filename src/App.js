2f32639ec77e7bbbb9dd70469292ceeb8214b05d
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2020-09-03 15:32:02 -0400

    fixing stable version

--- src/App.js
+++ src/App.js
@@ -262,6 +262,7 @@ class App extends Component {
 
     if (web3.networkId === this.state.MAIN_NETWORK_ID) {
       // mainnet
+      //URL = "https://api.compound.finance/api/v2/account";
       URL = "http://127.0.0.1:8080";
     } else if (web3.networkId === this.state.STAGING_NETWORK_ID) {
       // Staging
